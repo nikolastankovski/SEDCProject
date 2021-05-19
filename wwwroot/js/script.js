c6c23edbdcb6a13287d02d8f638bc70339df222d
Nikola Stankovski <stankovski.n@hotmail.com>
2021-05-20 00:07:18 +0200

    .

--- wwwroot/js/script.js
+++ wwwroot/js/script.js
@@ -57,41 +57,26 @@ const data = [
     {
         "date": "15.05.2021",
         "tableNo": 5,
-        "stars": 1,
+        "stars": 5,
         "comment": "test comment 10"
     }
 ];
+
 const columns = ["Date", "Table", "Stars", "Comment"];
 const tableContainer = document.getElementById("table-container");
-generateTable(data, columns);
+generateTable(data);
 
-function generateTable(data, columns) {
-    const table = document.createElement("table");
-    const thead = document.createElement("thead");
-    const tbody = document.createElement("tbody");
-
-    table.setAttribute("id", "table");
-    table.classList.add("table", "table-striped", "table-bordered", "table-responsive-sm");
-
-    table.appendChild(thead);
-    table.appendChild(tbody);
-
-    let headerRow = document.createElement("tr");
-
-    columns.forEach(item => {
-        let th = document.createElement("th");
-            th.classList.add("tableHeader");
-        th.textContent = item;
-        headerRow.appendChild(th);
-    });
-
-    thead.appendChild(headerRow);
+function generateTable(data) {
+    const tbody = document.getElementById("tableBody");
 
     data.forEach(item => {
         let tableRow = document.createElement("tr");
             Object.values(item).forEach((val, index) => {
             let td = document.createElement("td");
-            if(index == 2){
+            if(index == 1){
+                td.textContent = "Table " + val;
+            }
+            else if(index == 2){
                 for(let i = 1; i <= val; i++){
                     let star = document.createElement("i");
                     star.classList.add("fas", "fa-star");
@@ -106,9 +91,7 @@ function generateTable(data, columns) {
             tableRow.appendChild(td);
         })
         tbody.append(tableRow);
-    })
-    tableContainer.appendChild(table);
-    
+    })    
 }
 //------------------------------------------------------------------
 
@@ -119,8 +102,36 @@ const fourStarTotal = document.getElementById("4-star-total");
 const threeStarTotal = document.getElementById("3-star-total");
 const twoStarTotal = document.getElementById("2-star-total");
 const oneStarTotal = document.getElementById("1-star-total");
+const columnDate = document.getElementById("columnDate");
+const columnTable = document.getElementById("columnTable");
+const columnStars = document.getElementById("columnStars");
+const columnComment = document.getElementById("columnComment");
 
 
+totalCustomers.textContent = data.length;
 
-totalCustomers.textContent = "26";
-averageFeedbackScore.textContent = "4.8";
+//---- Calc Average -------------------------------------------------
+(() => {
+    let arrayStars = [];
+    for(let x of data){
+        arrayStars.push(x.stars);
+    }
+    let average = arrayStars.reduce((a,b) => a + b, 0) / data.length;
+    averageFeedbackScore.textContent = average
+})();
+// -------------------------------------------------------------------
+
+columnDate.addEventListener("click", () => {
+    console.log("date clicked");
+})
+columnTable.addEventListener("click", () => {
+    console.log("table clicked");
+})
+columnStars.addEventListener("click", () => {
+    console.log("stars  clicked");
+})
+columnComment.addEventListener("click", () => {
+    console.log("comment clicked");
+})
+
+
