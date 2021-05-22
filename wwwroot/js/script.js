819a008fcb65eec045f8ce05f6fb8525ee5b4fac
Nikola Stankovski <stankovski.n@hotmail.com>
2021-05-22 17:49:25 +0200

    added new style

--- wwwroot/js/script.js
+++ wwwroot/js/script.js
@@ -1,4 +1,4 @@
-//----------------- Table ------------------------------------------
+//----------------- Data ------------------------------------------
 let data = [
   {
     date: new Date("2021-05-21"),
@@ -73,17 +73,135 @@ let data = [
     comment: "test comment 10",
   },
 ];
+//------------------------------------------------------------------
 
-const columns = ["Date", "Table", "Stars", "Comment"];
-// const dataKeys = ["date", "tableNo", "stars", "comment"];
+//----------- Calc Average Score -------------------------
+(() => {
+    const averageFeedbackScore = document.getElementById("average-feedback-score");
+    let arrayStars = [];
+    for (let x of data) {
+        arrayStars.push(x.stars);
+    }
+    let average = (arrayStars.reduce((a, b) => a + b, 0) / data.length).toFixed(2);
+    averageFeedbackScore.textContent = average
+})();
+//--------------------------------------------------------
+
+//----------- Fill progress bars -------------------------
+(() => {
+    const fiveStarTotal = document.getElementById("5-star-total");
+    const fourStarTotal = document.getElementById("4-star-total");
+    const threeStarTotal = document.getElementById("3-star-total");
+    const twoStarTotal = document.getElementById("2-star-total");
+    const oneStarTotal = document.getElementById("1-star-total");
+
+    // parse ratings from 'data'
+    let arr = [fiveStarTotal, fourStarTotal, threeStarTotal, twoStarTotal, oneStarTotal];
+    let ctr = [0, 0, 0, 0, 0];
+    let total = 0;
+    data.map(x => x.stars).forEach(x => ctr[x - 1]++);
+    ctr.reverse();
+    for (let i = 0; i < 5; i++) {
+        arr[i].textContent = ctr[i];
+        total += ctr[i];
+    }
+
+    let rows = document.getElementById("star-feedback-container").getElementsByClassName("row");
+
+    for (let i = 0; i < 5; i++) {
+        rows[i].querySelector(".progress-bar:first-child").setAttribute("aria-valuemax", total);
+        rows[i].querySelector(".progress-bar:first-child").setAttribute("aria-valuenow", arr[i].textContent);
+        rows[i].querySelector(".progress-bar:first-child").style.width = `${parseFloat(arr[i].textContent) * 100 / total}%`;
+    }
+})();
+//--------------------------------------------------------
+//--------------------------------------------------------
+(() => {
+    document.getElementById("total-customers").textContent = data.length; // Fill Total Clients
+    sortData("date", "desc"); // initial sort by date
+    generateTable(data); // initial draw table
+})();
+//--------------------------------------------------------
+
 const sortStatus = ["desc", "asc", "asc"];
-const tableContainer = document.getElementById("table-container");
-generateTable(data);
 
 // get headers of sortable columns and bind k
 const columnDate = document.getElementById("columnDate");
 const columnTable = document.getElementById("columnTable");
 const columnStars = document.getElementById("columnStars");
+<<<<<<< HEAD
+
+function generateTable(data) {
+    const tbody = document.getElementById("tableBody");
+    tbody.innerHTML = "";
+
+    data.forEach(item => {
+        let tableRow = document.createElement("tr");
+        Object.values(item).forEach((val, index) => {
+            let td = document.createElement("td");
+            if (index == 0) {
+                td.textContent = val.toDateString();
+            }
+            else if (index == 1) {
+                td.textContent = "Table " + val;
+            }
+            else if (index == 2) {
+                for (let i = 1; i <= val; i++) {
+                    let star = document.createElement("i");
+                    star.classList.add("fas", "fa-star");
+                    td.appendChild(star);
+
+                }
+            }
+            else {
+                td.textContent = val;
+            }
+            tableRow.appendChild(td);
+        })
+        tbody.append(tableRow);
+    })
+}
+
+function toggleStatus(status) {
+    return status == "asc" ? "desc" : "asc";
+}
+
+function toggleArrow(col, sortOrder) {
+    const down = ".fa-long-arrow-alt-down";
+    const up = ".fa-long-arrow-alt-up";
+
+    let arr_down = col.querySelector(down);
+    arr_down.style.display = sortOrder === "asc" ? "none" : "";
+    let arr_up = col.querySelector(up);
+    arr_up.style.display = sortOrder === "desc" ? "none" : "";
+}
+
+function sortData(sortByKey, asc) {
+    data.sort((a, b) => {
+        if (asc == "asc") return a[sortByKey] - b[sortByKey];
+        return b[sortByKey] - a[sortByKey];
+    });
+}
+
+columnDate.addEventListener("click", (e) => {
+    sortStatus[0] = toggleStatus(sortStatus[0]);
+    toggleArrow(columnDate, sortStatus[0]);
+    sortData("date", sortStatus[0]);
+    generateTable(data);
+})
+columnTable.addEventListener("click", (e) => {
+    sortStatus[1] = toggleStatus(sortStatus[1]);
+    toggleArrow(columnTable, sortStatus[1]);
+    sortData("tableNo", sortStatus[1]);
+    generateTable(data);
+})
+columnStars.addEventListener("click", (e) => {
+    sortStatus[2] = toggleStatus(sortStatus[2]);
+    toggleArrow(columnStars, sortStatus[2]);
+    sortData("stars", sortStatus[2]);
+    generateTable(data);
+})
+=======
 
 //dark mode
 const darkModeswitch = document.getElementById("flexSwitchCheckDefault");
@@ -225,3 +343,4 @@ function fillProgressBar() {
 }
 
 fillProgressBar();
+>>>>>>> 0a1cdd922baf5484a1406a053434a36b2584bd04
